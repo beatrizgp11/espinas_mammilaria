dbe1550a2d7b870bdf77c8ae9f21b511313b89a3
Cristian Cervantes <cristoichkov@gmail.com>
2020-09-15 12:43:33 -0500

    Estadistica descriptiva

--- bin/stat_esp_cen.r
+++ bin/stat_esp_cen.r
@@ -1,6 +1,10 @@
 ## Este scrpt es para subir todos los archivos .csv de espinas centrales
 
 library(tidyverse)
+library(pander)
+library(qqplotr)
+library(ggpubr)
+library(formattable)
 
 ## funcion para leer archivos .csv de espinas centrales
 source("read_esp_cen.R")
@@ -35,3 +39,49 @@ for (i in local) {
 
 
 df_esp_cen <- separate(df_esp_cen, id, c("Localidad", "Planta", "Areola"), sep = "_")
+
+
+head(df_esp_cen)
+
+stats <- df_esp_cen %>%
+  dplyr::group_by(Localidad, posicion) %>%
+  summarise(media = mean(long_esp), sd = sd(long_esp))
+
+
+pander(stats, style = 'rmarkdown')
+
+formattable(stats)
+
+
+hist <- df_esp_cen %>%  
+  filter(Localidad == "CC020") %>%
+  ggplot(aes(long_esp)) +
+  geom_histogram(aes(y=..density.., fill=posicion), color = "grey30") +
+  facet_grid(posicion~.) +
+  labs(x = "Longitud de espina central (mm)", y = "Densidad")
+
+
+  qq <- df_esp_cen %>%
+    filter(Localidad == "CC020") %>%
+    ggplot(aes(sample = long_esp)) +
+    stat_qq_band(alphta=0.5) +
+    stat_qq_line() +
+    stat_qq_point() +
+    facet_grid(posicion~.) +
+    labs(x = "Quantiles teoricos", y = "Quantiles muestra")
+
+  
+  bp <- df_esp_cen %>%
+    filter(Localidad == "CC020") %>%
+    ggplot(aes(x = posicion, y = long_esp)) +
+    geom_boxplot() +
+    labs(x = "Posicion", y = "Longiotud de espina central (mm)")
+  
+  
+  figura <- ggarrange(hist, qq, bp, labels = c("A", "B", "C"))
+
+ figura_f <- annotate_figure(figura, top = text_grob("Espinas centrales localidad CC021", face = "bold", size = 14),
+                  fig.lab = "Figura 1", fig.lab.size = 14, fig.lab.face = "bold")
+ 
+ 
+ ggsave(figura_f, file="../out/est_descriptiva/Loc_CC020.png", device="png", dpi = 300, width = 14, height = 12)
